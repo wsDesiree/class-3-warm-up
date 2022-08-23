b04074bad56d27da3325b87c74ed710126ec5020
wsDesiree <110789227+wsDesiree@users.noreply.github.com>
2022-08-22 20:21:35 -0500

    exercise

---
+++ part-2-class-3/excerise1.js
@@ -0,0 +1,7 @@
+function vanillacake(eggs, flour, sugar){
+    console.log(flour); 
+    console.log(eggs);
+    console.log(sugar);
+    console.log('vanillacake'+ ' contains all these items!')
+} 
+vanillacake("eggs", "flour", "sugar");
