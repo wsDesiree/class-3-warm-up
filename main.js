e6fdd2fad0c5b598d35a4f8348d5df79d8424114
wsDesiree <110789227+wsDesiree@users.noreply.github.com>
2022-08-25 17:48:12 -0500

    warmup-3
    findwaldo

---
+++ main.js
@@ -0,0 +1,14 @@
+const findWaldo(['Stacy', 'Waldo', 'John']) 
+const findWaldo(['Waldo', 'Jimmy', 'James', 'Jamie', 'Waldo']) // ['Found Waldo!', 'Jimmy', 'James', 'Jamie', 'Found Waldo!']
+
+
+function findWaldo(group) {
+ let people =['Stacy', 'Waldo', 'John']
+ for (let i = ; i < people.length)
+
+ ]
+}
+
+findWaldo(['Stacy', 'Waldo', 'John']) // returns ['Stacy', 'Found Waldo!', 'John']
+findWaldo(['Waldo', 'Jimmy', 'James', 'Jamie', 'Waldo']) // ['Found Waldo!', 'Jimmy', 'James', 'Jamie', 'Found Waldo!']
+
