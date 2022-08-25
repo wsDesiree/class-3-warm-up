ce042e5885910de5dad7f1121d80dde216b98987
wsDesiree <110789227+wsDesiree@users.noreply.github.com>
2022-08-25 17:58:10 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-const findWaldo(['Stacy', 'Waldo', 'John']) 
-const findWaldo(['Waldo', 'Jimmy', 'James', 'Jamie', 'Waldo']) // ['Found Waldo!', 'Jimmy', 'James', 'Jamie', 'Found Waldo!']
-
-
 function findWaldo(group) {
- let people =['Stacy', 'Waldo', 'John']
- for (let i = ; i < people.length)
-
- ]
+ let newArr
+ for (let i = 0; i < group.length;i++){
+    if(group[i] === 'Waldo'){
+        newArr.push("I found Waldo!")
+        }else{
+            newArr.push(group[i])
+        }
+ }
+ return newArr;
 }
 
 findWaldo(['Stacy', 'Waldo', 'John']) // returns ['Stacy', 'Found Waldo!', 'John']
