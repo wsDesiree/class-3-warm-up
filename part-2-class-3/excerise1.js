282b7836fbf283e88c4d83fe5c603a8e679814c5
wsDesiree <110789227+wsDesiree@users.noreply.github.com>
2022-08-25 13:40:33 -0500

    exerciseshomework
    working on writing functions and making other things happen as well.

--- part-2-class-3/excerise1.js
+++ part-2-class-3/excerise1.js
@@ -2,6 +2,6 @@ function vanillacake(eggs, flour, sugar){
     console.log(flour); 
     console.log(eggs);
     console.log(sugar);
-    console.log('vanillacake'+ ' contains all these items!')
+    console.log('vanillacake'+ ' are all igredients to making tasty cake!')
 } 
 vanillacake("eggs", "flour", "sugar");
